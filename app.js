d825e3fe9d8d6663ce2f02c57dfa9d4f64ab0c03
kylegallard <kylegallard@gmail.com>
2021-05-25 20:09:54 +1000

    Added about and contact page using EJS/ Node/ Express. Need to set up tab links on click next.

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ app.get("/", function (req, res) {
   res.render("home", { homeContent: homeStartingContent });
 })
 
+app.get("/about", function (req, res) {
+  res.render("about", { about: aboutContent });
+})
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contact: contactContent });
+})
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
